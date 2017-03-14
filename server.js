c05bd459c4dfb24c678bb3b891e8251dc54dd82b
Fernando Hernandez <fernando.hdz97@gmail.com>
2017-03-13 17:19:27 -0700

    Routes

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 /**
  * Created by jpichardo on 2/18/17.
  */
+var express = require("express");
+var app = express();
+var Order = require("./src/models/order");
+
+app.get("/api/orders", function(req,res){
+  console.log("Metodo GET");
+  res.send("Order");
+})
+
+app.get("api/orders/:order_id", function(req,res){
+  Order.find(function (err, orders) {
+    if (err) {
+      res.send(err);
+    }
+    else {
+      res.json(orders);
+    }
+  })
+})
+
+app.post("api/orders", function (req,res) {
+  console.log("HOLAAA");
+  res.send("Metodo POST");
+})
+
+app.listen(8080);
--- src/models/order.js
+++ src/models/order.js
@@ -5,8 +5,8 @@ var moongose = require("mongoose");
 var Schema = moongose.Schema;
 
 var orderSchema = new Schema({
-  {
-    id: ObjectId,
+
+    id: Schema.Types.ObjectId,
     details: [{type: Array, required: true,
       product: {
         name: String,
@@ -15,13 +15,13 @@ var orderSchema = new Schema({
           servings: Number,
           discountPercentage: Number
         }
-      }
+      },
       quantity: Number
     }],
     orderDate: {type: Date, required: true},
     deliveryDate: {type: Date, required: true},
     client: { type: Schema.Types.ObjectId, ref: "Client"}
-  }
+
 });
 
 module.exports = moongose.model("Order", orderSchema);
