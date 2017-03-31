4d4b3fd740b0b3aa10387526e1d1841a5c2527b4
Fernando Hernandez <fernando.hdz97@gmail.com>
2017-03-31 09:10:24 -0700

    Update order

--- src/models/order.js
+++ src/models/order.js
@@ -6,20 +6,20 @@ var Schema = moongose.Schema;
 
 var orderDetailSchema = new Schema({
   product: {
-    name: {type: String, required: true},
-    unitPrice: {type: Number, required: true},
+    name: {type: String, required: [true, "Ingresa el nombre del producto porfavor"]},
+    unitPrice: {type: Number, required: [true, "Ingresa el precio unitario del producto"]},
     presentation: {
-      servings: {type: Number, required: true},
-      discountPercentage: {type: Number, required: true}
+      servings: {type: Number, required: [true, "Ingresa la cantidad de porciones"], min: [10, "La cantidad minima es 10"]},
+      discountPercentage: {type: Number, min: [1, "El descuento minimo es 1"], max: [100, "El descuento maximo es 100"]}
     }
   },
-  quantity: {type: Number, required: true}
+  quantity: {type: Number, required: [true, "Ingresa el numero de productos"], min:[1, "El numero minimo de productos es 1"]}
 })
 
 var orderSchema = new Schema({
     details: [orderDetailSchema],
-    orderDate: {type: Date, required: true},
-    deliveryDate: {type: Date, required: true},
+    orderDate: {type: Date, required: [true, "Ingresa la fecha de orden"]},
+    deliveryDate: {type: Date, required: [true, "Ingresa la fecha de entrega"]},
     client: { type: Schema.Types.ObjectId, ref: "Client"}
 
 });
