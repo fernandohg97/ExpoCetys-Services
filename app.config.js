2d3cdd1c9c8ba7918355951daeb3826d3869e695
Fernando Hernandez <fernando.hdz97@gmail.com>
2017-04-29 12:08:23 -0700

    Change mongodb variable

--- app.config.js
+++ app.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 8482,
-  db: process.env.MONGODB || 'mongodb://localhost/',
+  db: process.env.MONGODB_URI || 'mongodb://localhost/',
   SECRET_TOKEN: 'miclavedetokens'
 }
