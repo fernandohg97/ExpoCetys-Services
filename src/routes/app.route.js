ee173abc659a8efcbcd1de87fe9a3dd03301318f
Fernando Hernandez <fernando.hdz97@gmail.com>
2017-05-17 17:49:33 -0700

    changes

--- src/routes/app.route.js
+++ src/routes/app.route.js
@@ -1,11 +1,11 @@
 'use strict'
 const express = require('express')
 const orderRouter = express.Router()
-// const productRouter = express.Router()
-// const clientRouter = express.Router()
+const productRouter = express.Router()
+const clientRouter = express.Router()
 const OrderCtrl = require('../controllers/order.controller')
-// const ProductCtrl = require('../controllers/product.controller')
-// const ClientCtrl = require('../controllers/client.controller')
+const ProductCtrl = require('../controllers/product.controller')
+const ClientCtrl = require('../controllers/client.controller')
 const UserCtrl = require('../controllers/user.controller')
 const auth = require('../middlewares/auth')
 
@@ -38,36 +38,36 @@ orderRouter.post('/signin', UserCtrl.signIn)
 
 // PRODUCTS
 // get all the products
-// productRouter.get('/products', ProductCtrl.getProducts)
-//
-// // get a single product
-// productRouter.get('/products/:product_id', ProductCtrl.getProduct)
-//
-// // create a product
-// productRouter.post('/products', ProductCtrl.createProduct)
-//
-// // update a product
-// productRouter.put('/products/:product_id', ProductCtrl.updateProduct)
-//
-// // delete a product
-// productRouter.delete('/products/:product_id', ProductCtrl.removeProduct)
-//
-// // CLIENTS
-// // get all the clients
-// clientRouter.get('/clients', ClientCtrl.getClients)
-//
-// // get a single client
-// clientRouter.get('/clients/:client_id', ClientCtrl.getClient)
-//
-// // create a client
-// clientRouter.post('/clients', ClientCtrl.createClient)
-//
-// // update a client
-// clientRouter.put('/clients/:client_id', ClientCtrl.updateClient)
-//
-// // delete a client
-// clientRouter.delete('/clients/:client_id', ClientCtrl.removeClient)
+productRouter.get('/products', ProductCtrl.getProducts)
+
+// get a single product
+productRouter.get('/products/:product_id', ProductCtrl.getProduct)
+
+// create a product
+productRouter.post('/products', ProductCtrl.createProduct)
+
+// update a product
+productRouter.put('/products/:product_id', ProductCtrl.updateProduct)
+
+// delete a product
+productRouter.delete('/products/:product_id', ProductCtrl.removeProduct)
+
+// CLIENTS
+// get all the clients
+clientRouter.get('/clients', ClientCtrl.getClients)
+
+// get a single client
+clientRouter.get('/clients/:client_id', ClientCtrl.getClient)
+
+// create a client
+clientRouter.post('/clients', ClientCtrl.createClient)
+
+// update a client
+clientRouter.put('/clients/:client_id', ClientCtrl.updateClient)
+
+// delete a client
+clientRouter.delete('/clients/:client_id', ClientCtrl.removeClient)
 
 module.exports = orderRouter
-// module.exports = productRouter
-// module.exports = clientRouter
+module.exports = productRouter
+module.exports = clientRouter
